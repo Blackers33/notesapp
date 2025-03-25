ad948aaa9cad31a151fe1ea21ca2e128c84a1d8b
Sacha <sacha.alt@proton.me>
2025-03-25 15:01:39 +0100

    FEATURE Added Icons, fixed X button position

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,15 +1,12 @@
 import Link from "next/link";
 import React from "react";
-
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 function Navbar(props) {
 	return (
 		<nav className='bg-white border-gray-200 dark:bg-gray-900 shadow-lg'>
 			<div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
-				<a
-					href='#'
-					className='flex items-center space-x-3 rtl:space-x-reverse'
-				>
+				<a href='#' className='flex items-center space-x-3 rtl:space-x-reverse'>
 					<img
 						src='https://www.pngfind.com/pngs/m/572-5721612_accounting-report-comments-notes-transparent-icon-hd-png.png'
 						className='h-8'
@@ -19,31 +16,23 @@ function Navbar(props) {
 						Notes app
 					</span>
 				</a>
-				<button
-					data-collapse-toggle='navbar-default'
-					type='button'
-					className='inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
-					aria-controls='navbar-default'
-					aria-expanded='false'
-				>
-					<span className='sr-only'>Open main menu</span>
-					<svg
-						className='w-5 h-5'
-						aria-hidden='true'
-						xmlns='http://www.w3.org/2000/svg'
-						fill='none'
-						viewBox='0 0 17 14'
+
+				<div className='flex space-x-8'>
+					<Link
+						href='https://github.com/Blackers33'
+						target='_blank'
+						rel='noopener noreferrer'
 					>
-						<path
-							stroke='currentColor'
-							strokeLinecap='round'
-							strokeLinejoin='round'
-							strokeWidth='2'
-							d='M1 1h15M1 7h15M1 13h15'
-						/>
-					</svg>
-				</button>
-			
+						<FaGithub className='text-gray-600 dark:text-white text-2xl hover:text-gray-800 dark:hover:text-gray-400' />
+					</Link>
+					<Link
+						href='https://www.linkedin.com/in/sachaaltaber/'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						<FaLinkedin className='text-gray-600 dark:text-white text-2xl hover:text-blue-600' />
+					</Link>
+				</div>
 			</div>
 		</nav>
 	);
--- components/Note.jsx
+++ components/Note.jsx
@@ -21,34 +21,19 @@ import {
 	CardTitle,
 } from "@/components/ui/card";
 
-
-
-
-// <Note note={noteData} />
-export default function Note({note, className = '', onDeleteNote}) {
-
+export default function Note({ note, className = "", onDeleteNote }) {
 	const date = new Date(note.date).toDateString();
 
 	return (
-		<Card className={`w-[350px] ${note.style.background} ${className}`}>
-			<CardHeader>
-				<CardTitle>
-					{/* Shows title if any, shows "Untitled" otherwise */}
-					{note.title ? (
-						<span className={`${note.style.title}`}>{note.title}</span>
-					) : (
-						<span className='text-gray-300 italic'>Untitled</span>
-					)}
-				</CardTitle>
-				<CardDescription>{date}</CardDescription>
-			</CardHeader>
-			<CardContent className={`${note.style.content}`}>
-				{note.content}
-			</CardContent>
-			<CardFooter className='flex justify-between'>
+		<Card
+			className={`relative w-[350px] ${note.style.background} ${className}`}
+		>
+			<div className='absolute top-2 right-2'>
 				<AlertDialog>
 					<AlertDialogTrigger asChild>
-						<Button variant='outline'>X</Button>
+						<Button variant='outline' size='icon'>
+							X
+						</Button>
 					</AlertDialogTrigger>
 					<AlertDialogContent>
 						<AlertDialogHeader>
@@ -65,7 +50,19 @@ export default function Note({note, className = '', onDeleteNote}) {
 						</AlertDialogFooter>
 					</AlertDialogContent>
 				</AlertDialog>
-			</CardFooter>
+			</div>
+			<CardHeader>
+				<CardTitle>
+					{note.title ? (
+						<span className={note.style.title}>{note.title}</span>
+					) : (
+						<span className='text-gray-300 italic'>Untitled</span>
+					)}
+				</CardTitle>
+				<CardDescription>{date}</CardDescription>
+			</CardHeader>
+			<CardContent className={note.style.content}>{note.content}</CardContent>
+			<CardFooter />
 		</Card>
 	);
 }
